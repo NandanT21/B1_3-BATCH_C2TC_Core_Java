3cc8c2a8498bad5c731ac53c26ff851e7974f782
Nandan-Thyagaraj <nandanthyagaraj21@github.com>
2022-05-16 00:25:21 +0530

    FIRST COMMIT

---
+++ Day14_Strings/src/net/cg/strings/Example1.java
@@ -0,0 +1,15 @@
+package net.cg.strings;
+
+import java.util.Scanner;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str1=s.nextLine();
+		String str2=s.nextLine();
+		System.out.println(str1.equals(str2));
+
+	}
+
+}
---
+++ Day14_Strings/src/net/cg/strings/Example4.java
@@ -0,0 +1,18 @@
+package net.cg.strings;
+
+public class Example4 {
+
+	public static void main(String[] args) {
+		String str="Nandan";
+		String str1="Thyagaraj";
+		System.out.println(str.charAt(4));
+		System.out.println(str.toLowerCase());
+		System.out.println(str.toUpperCase());
+		System.out.println(str+" "+str1);
+		System.out.println(str+" ".concat(str1));
+		String s = 50+20+50 + "error" +"Nandhan"+ 50 + 50 + 20+"Thyagaraju";  
+		System.out.println(s);
+		
+	}
+
+}

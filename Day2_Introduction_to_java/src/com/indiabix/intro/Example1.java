4cd49dedc4754a617f646bc80948ef3ae103351a
Nandan-Thyagaraj <nandanthyagaraj21@github.com>
2022-05-15 21:24:55 +0530

    FIRST COMMIT

---
+++ Day2_Introduction_to_java/src/com/indiabix/intro/Example1.java
@@ -0,0 +1,19 @@
+package com.indiabix.intro;
+
+public class Example1 {
+
+	public static void main(String[] args) 
+	{
+		int a=21;//whole
+		float b=18.2f;//decimal point
+		double c=14.5;//decimal point
+		char d='a';
+		String str="Nandan Thyagaraj";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(str);
+	}
+
+}
---
+++ Day2_Introduction_to_java/src/com/indiabix/intro/Example5.java
@@ -0,0 +1,16 @@
+package com.indiabix.intro;
+
+import java.util.Scanner;
+
+public class Example5 {
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		char ch = in.next().charAt(0);
+		System.out.println(ch);
+		in.close();
+
+	}
+
+}

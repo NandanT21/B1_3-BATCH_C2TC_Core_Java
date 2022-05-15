084b4cc25b0318e8526837fc941b607409d7a0eb
Nandan-Thyagaraj <nandanthyagaraj21@github.com>
2022-05-15 23:37:13 +0530

    FIRST COMMIT

---
+++ Day7_this_static_instanceof/src/com/indiabix/static1/Example3.java
@@ -0,0 +1,23 @@
+package com.indiabix.static1;
+
+public class Example3 
+{
+	static int x;
+	static String str;
+	static
+	{
+		x=10;
+		str="Shiwani";
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(x);
+		System.out.println(str);
+
+	}
+
+}
